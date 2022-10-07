fb48ae8dd4e957f728c50dcecb8b0a5507bb467a
Ben Ashbaugh <ben.ashbaugh@intel.com>
2022-10-06 22:52:59 -0700

    add a patch for Windows HKR, disable OpenCLon12

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -12,7 +12,9 @@ sources = [
     FileSource("https://patch-diff.githubusercontent.com/raw/KhronosGroup/OpenCL-Headers/pull/209.patch",
                "c3afd4ad0a37f0b61c0b8656ca4914002ba7994bba05aa2c47fde59b652289c9"),
     FileSource("https://patch-diff.githubusercontent.com/raw/KhronosGroup/OpenCL-ICD-Loader/pull/185.patch",
-               "82725e3ec4e9fe333aeb53f75e13b74cef22c7cb662ee2af51108f0d764e1985")
+               "82725e3ec4e9fe333aeb53f75e13b74cef22c7cb662ee2af51108f0d764e1985"),
+    FileSource("https://patch-diff.githubusercontent.com/raw/KhronosGroup/OpenCL-ICD-Loader/pull/186.patch",
+               "6d1ce0e59e53405ff9a677d301c853e6b8de56b47ba2dd8f6f1e6562229cc66e")
 ]
 
 # Bash recipe for building across all platforms
@@ -23,11 +25,12 @@ install_license ./OpenCL-Headers/LICENSE
 
 patch ./OpenCL-Headers/tests/test_headers.c 209.patch
 patch ./OpenCL-ICD-Loader/loader/icd_platform.h 185.patch
+patch ./OpenCL-ICD-Loader/loader/windows/icd_windows_hkr.c 186.patch
 
 cmake -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -S ./OpenCL-Headers -B ./OpenCL-Headers/build
 cmake --build ./OpenCL-Headers/build --target install -j${nproc}
 
-cmake -DCMAKE_PREFIX_PATH=${prefix} -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -S ./OpenCL-ICD-Loader -B ./OpenCL-ICD-Loader/build
+cmake -DCMAKE_PREFIX_PATH=${prefix} -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DOPENCL_ICD_LOADER_DISABLE_OPENCLON12=ON -S ./OpenCL-ICD-Loader -B ./OpenCL-ICD-Loader/build
 cmake --build ./OpenCL-ICD-Loader/build --target install -j${nproc}
 """
 
